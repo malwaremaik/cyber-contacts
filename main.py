17d69d96ea815eab0b2d36a99c0fdcba2d45c051
Maik Cawelius <maik.cawelius@icloud.com>
2025-02-14 11:48:56 +0100

    feat: store contacts in a list of dictionaries

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+# All contacts
+contact_list = []
+
 # Get contact information
 name = input("Enter a name: ")
 email = input("Enter an email: ")
 phone_number = input("Enter a phone number: ")
 
-print(name, email, phone_number)
+contact_list.append({ "name": name, "email": email, "phone number": phone_number })
+
+print(contact_list)
